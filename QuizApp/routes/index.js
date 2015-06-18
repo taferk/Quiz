eb893defed1710ee32533cd1dfea8758022735a5
Aurelio García <taferk@gmail.com>
2015-06-18 18:47:18 +0200

    Se añade la sección de creditos sin el video (es opcional)

--- QuizApp/routes/index.js
+++ QuizApp/routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
+router.get('/author', function (req, res) {
+    res.render('creditos', { author: 'Aurelio García' });
+});
+
+router.get('/quizes', function (req, res) {
+    res.render('quizes/question', { pregunta: 'Aún sin implementar' });
+});
+
+router.get('/answer', function (req, res) {
+    res.render('quizes/answer', { resultado: 'Aún sin implementar' });
+});
+
+
 module.exports = router;
