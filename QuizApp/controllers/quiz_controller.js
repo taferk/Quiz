4b229cdf4676a3f5310154de5e37fbcaccd3b3ae
Aurelio García <taferk@gmail.com>
2015-06-20 11:27:19 +0200

    Se crean los modelos y el controlador

---
+++ QuizApp/controllers/quiz_controller.js
@@ -0,0 +1,17 @@
+﻿var models = require('../models/models');
+
+exports.question = function (req, res) {
+    models.Quiz.findAll().then(function (quiz) {
+            res.render('quizes/question', { pregunta: quiz[0].pregunta})
+    })
+};
+
+exports.answer = function (req, res) {
+    models.Quiz.findAll().then(function (quiz) {
+        if (req.query.respuesta === quiz[0].respuesta) {
+            res.render('quizes/answer', { respuesta: 'Correcto' });
+        } else {
+            res.render('quizes/answer', { respuesta: 'Incorrecto' });
+        }
+    })
+};
